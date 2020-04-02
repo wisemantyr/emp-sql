52e212ab8e3b12f75dcc6948dc4b56486bb3a1b7
wisemantyr <wisemantyr@gmail.com>
2020-04-02 18:08:12 -0400

    created tables and imported csv files

---
+++ EmployeeSQL/EmployeeSQL.sql
@@ -0,0 +1,47 @@
+create table employees (
+emp_no int primary key,
+birth_date date,
+first_name varchar,
+last_name varchar,
+gender varchar,
+hire_date date
+);
+
+create table departments (
+dept_no varchar primary key,
+dept_name varchar
+);
+
+create table dept_emp (
+emp_no int,
+dept_no varchar,
+from_date date,
+to_date date,
+foreign key (emp_no) references employees(emp_no),
+foreign key (dept_no) references departments(dept_no)
+);
+
+create table dept_manager (
+dept_no varchar,
+emp_no int,
+from_date date,
+to_date date,
+foreign key (dept_no) references departments (dept_no),
+foreign key (emp_no) references employees (emp_no)
+);
+
+create table salaries (
+emp_no int,
+salary int,
+from_date date,
+to_date date,
+foreign key (emp_no) references employees (emp_no)
+);
+
+create table titles (
+emp_no int,
+title varchar,
+from_date date,
+to_date date,
+foreign key (emp_no) references employees (emp_no)
+);
